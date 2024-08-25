59f42ddd9f1ebadd23344a2981256bdeb3705cc3
Anand P <anandpamegoze@gmail.com>
2024-08-25 17:07:29 +0530

    style video background styling in browse page Main container

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -5,12 +5,16 @@ const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useMovieTrailer(movieId);
   return (
-    <div>
+    <div className="w-screen">
       <iframe
-        className=""
-        width="560"
-        height="315"
-        src={'https://www.youtube.com/embed/' + trailerVideo?.key}
+        className="w-full aspect-video"
+        src={
+          'https://www.youtube.com/embed/' +
+          trailerVideo?.key +
+          '?playlist=' +
+          trailerVideo?.key +
+          '&loop=1&autoplay=1&mute=1&controls=0&modestbranding=1&showinfo=0'
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 pl-10 pr-6">
+    <div className="absolute w-screen pt-[18%] aspect-video pl-24 pr-6 text-white bg-gradient-to-r from-black">
       <h1 className="text-3xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-2/4">{overview}</p>
+      <p className="py-6 text-lg w-1/4">{overview}</p>
       <div>
-        <button className="bg-opacity-50 bg-gray-500 px-10 rounded-xl text-black p-4 text-xl">
-          ▶️ Play
+        <button className=" bg-white px-10 rounded-xl text-black p-4 text-xl hover:bg-opacity-80">
+          Play
         </button>
-        <button className="bg-opacity-50 bg-red-200 px-10 rounded-xl ml-2 text-black p-4 text-xl">
+        <button className="bg-opacity-50 bg-gray-500 px-10 rounded-xl ml-2 text-white p-4 text-xl">
           More Info
         </button>
       </div>
